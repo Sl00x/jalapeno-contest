6497f14a7d692b8ed157718363da376a417c2127
Benjamin Rousseliere <benjamin.rousseliere@gmail.com>
2023-03-14 22:32:14 +0100

    update

--- api/src/user/user.service.ts
+++ api/src/user/user.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  InternalServerErrorException,
+  Injectable,
+} from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { User, Prisma } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
@@ -8,13 +12,22 @@ export class UserService {
   constructor(private prisma: PrismaService) {}
 
   async createUser(data: Prisma.UserCreateInput) {
-    const { password, ...user } = await this.prisma.user.create({
-      data: {
-        ...data,
-        password: await bcrypt.hash(data.password, 10),
-        birthdate: new Date(data.birthdate),
-      },
-    });
+    const { password, ...user } = await this.prisma.user
+      .create({
+        data: {
+          ...data,
+          password: await bcrypt.hash(data.password, 10),
+          birthdate: new Date(data.birthdate),
+        },
+      })
+      .catch((error) => {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+          if (error.code === 'P2002') {
+            throw new ConflictException('Email déjà utilisé');
+          }
+        }
+        throw new InternalServerErrorException();
+      });
 
     return user;
   }
